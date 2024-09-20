88a4b78e3c3e7b8a875fa5b88a700d3a2660a689
Mark Vargas <mav49er@gmail.com>
2024-09-19 19:36:13 -0500

    Testing signature verification

--- src/main/java/com/markvargas/discordbot/client/discord/SlashCommandController.java
+++ src/main/java/com/markvargas/discordbot/client/discord/SlashCommandController.java
@@ -2,14 +2,32 @@ package com.markvargas.discordbot.client.discord;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.markvargas.discordbot.client.discord.model.Interaction;
+import java.io.ByteArrayInputStream;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.Provider;
 import java.security.PublicKey;
+import java.security.Security;
 import java.security.Signature;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.spec.EdECPoint;
+import java.security.spec.EdECPublicKeySpec;
+import java.security.spec.NamedParameterSpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Collections;
 import java.util.HexFormat;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.tomcat.util.buf.HexUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,13 +54,36 @@ public class SlashCommandController {
 
     byte[] byteKey = HexFormat.of().parseHex(PUBLIC_KEY);
     byte[] byteSignature = HexFormat.of().parseHex(signature);
-    X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(byteKey);
+//
     KeyFactory kf = KeyFactory.getInstance("Ed25519");
-    PublicKey publicKey = kf.generatePublic(x509EncodedKeySpec);
+    boolean xisodd = false;
+    int lastByteInt = byteKey[byteKey.length - 1];
+    if ((lastByteInt & 255) >> 7 == 1) {
+      xisodd = true;
+    }
+
+    byteKey[byteKey.length - 1] &= 127;
+    int i = 0;
+    int j = byteKey.length - 1;
+    byte temp;
+    while (j > i) {
+      temp = byteKey[j];
+      byteKey[j] = byteKey[i];
+      byteKey[i] = temp;
+      j--;
+      i++;
+    }
+    BigInteger y = new BigInteger(1, byteKey);
+
+    NamedParameterSpec paramSpec = new NamedParameterSpec("Ed25519");
+    EdECPoint ep = new EdECPoint(xisodd, y);
+    EdECPublicKeySpec publicKeySpec = new EdECPublicKeySpec(paramSpec, ep);
+    PublicKey publicKey = kf.generatePublic(publicKeySpec);
 
     Signature sig = Signature.getInstance("Ed25519");
     sig.initVerify(publicKey);
-    sig.update(data.getBytes(StandardCharsets.UTF_8));
+    byte[] message = data.getBytes(StandardCharsets.UTF_8);
+    sig.update(message);
     return sig.verify(byteSignature);
   }
 
