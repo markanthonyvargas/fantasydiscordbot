2cc2ffe521a470cc1ec48c4e776aca707d018bb9
markanthonyvargas <mav49er@gmail.com>
2024-09-22 15:18:54 -0500

    Removing try catch for discord messages so retry can take effect

--- src/main/java/com/markvargas/discordbot/client/discord/service/DiscordService.java
+++ src/main/java/com/markvargas/discordbot/client/discord/service/DiscordService.java
@@ -23,19 +23,15 @@ public class DiscordService {
   public void createMessage(String message) {
     log.info("Retry number: {}", RetrySynchronizationManager.getContext().getRetryCount());
     DiscordMessageRequest messageRequest = new DiscordMessageRequest(message);
-    try {
-      log.info("Attempting to post message to Discord channel");
-      ResponseEntity<String> response =
-          discordRestClient.post().body(messageRequest).retrieve().toEntity(String.class);
-      if (response.getStatusCode().is2xxSuccessful()) {
-        log.info("Message posted successfully!");
-      } else {
-        log.info(
-            "Message was not posted successfully to Discord, received the following response: {}",
-            response.getBody());
-      }
-    } catch (Exception e) {
-      log.error("Error occurred while attempting to post a message to Discord channel", e);
+    log.info("Attempting to post message to Discord channel");
+    ResponseEntity<String> response =
+        discordRestClient.post().body(messageRequest).retrieve().toEntity(String.class);
+    if (response.getStatusCode().is2xxSuccessful()) {
+      log.info("Message posted successfully!");
+    } else {
+      log.info(
+          "Message was not posted successfully to Discord, received the following response: {}",
+          response.getBody());
     }
   }
 }
