1533351cfe87a339f9149822693e70aea9a1745e
markanthonyvargas <mav49er@gmail.com>
2024-09-23 21:13:29 -0500

    Adding back retry logic for Discord calls

--- src/main/java/com/markvargas/discordbot/client/discord/service/DiscordService.java
+++ src/main/java/com/markvargas/discordbot/client/discord/service/DiscordService.java
@@ -5,6 +5,9 @@ import discord4j.core.GatewayDiscordClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
+import org.springframework.retry.support.RetrySynchronizationManager;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +19,9 @@ public class DiscordService {
 
   @Autowired private GatewayDiscordClient discordClient;
 
+  @Retryable(backoff = @Backoff(delay = 3000))
   public void createMessage(String message) {
+    log.info("Retry number: {}", RetrySynchronizationManager.getContext().getRetryCount());
     log.info("Attempting to post message to Discord channel");
     discordClient
         .getRestClient()
